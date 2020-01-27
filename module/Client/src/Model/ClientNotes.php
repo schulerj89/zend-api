460773e8a73011a7be2f27997b18ca255a29180e
Josh Schuler <Josh.Schuler@landmarktx.com>
2020-01-27 14:03:42 -0600

    dynamically fill object

--- module/Client/src/Model/ClientNotes.php
+++ module/Client/src/Model/ClientNotes.php
@@ -6,7 +6,8 @@ class ClientNotes
 {
     public function exchangeArray(array $data)
     {
-        $this->clientid  = !empty($data['clientid']) ? $data['clientid'] : null;
-        $this->notes 	 = !empty($data['notes']) ? $data['notes'] : null;
+    	foreach($data as $key => $_data) {
+        	$this->{$key}  = !empty($data[$key]) ? $data[$key] : null;
+    	}
     }
 }
